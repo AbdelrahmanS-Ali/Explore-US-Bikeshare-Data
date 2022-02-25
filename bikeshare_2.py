2f300302bdb5bbd22c4e25023ee80f358d92ef39
Abdelrahman Saeed Ali <asali@ejadaalex.local>
2022-02-25 17:53:14 +0300

    added show_data

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -175,6 +175,21 @@ def user_stats(df):
     print('-'*40)
 
 
+def show_data(df):
+    keep_showing_data = True
+    start_loc = 0
+    while keep_showing_data:
+        wants_data = input('Do you want to see the first 5 rows of data? (enter yes or no)\n').lower()
+        if wants_data == 'no':
+            break
+        elif wants_data == 'yes':
+            print(df.iloc[start_loc:start_loc+5])
+            start_loc += 5
+        else:
+            print('invalid input')
+
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -184,6 +199,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
+        show_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
@@ -193,6 +209,7 @@ def main():
 if __name__ == "__main__":
     city, month, day = get_filters()
     df = load_data(city, month, day)
+    show_data(df)
     time_stats(df)
     station_stats(df)
     trip_duration_stats(df)
